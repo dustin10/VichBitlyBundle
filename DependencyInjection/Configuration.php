cb9b88808e5a092e245a8fdfb19370ee7feb44a0
Dustin Dobervich <ddobervich@gmail.com>
2011-07-28 16:11:33 -0500

    fixed type in Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,7 +24,7 @@ class Configuration
         $root
             ->children()
                 ->scalarNode('login_name')->cannotBeOverwritten()->isRequired()->end()
-                ->scalarNode('api_key')->cannotBeOverwriteen()->isRequired()->end()
+                ->scalarNode('api_key')->cannotBeOverwritten()->isRequired()->end()
             ->end()
         ;
         
